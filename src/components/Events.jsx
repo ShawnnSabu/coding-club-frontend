1a4424594a12d1221a8ee2f9cfb890a392797300
Sreeresh-cloud <sreereshss2@gmail.com>
2025-02-14 12:00:41 +0530

    Resolved Registration Bug

--- src/components/Events.jsx
+++ src/components/Events.jsx
@@ -172,7 +172,7 @@ const Events = () => {
                             </div>
                         ))
                     ) : (
-                        <p>No events available</p>
+                        <p>Loading the events...Please Wait!</p>
                     )}
                 </div>
             </div>
